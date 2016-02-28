b791f13f93130c9b0b922a04fccfad85750bc9fa
Gavin Benda <gavin@nucleus.com.au>
2016-02-28 18:00:53 +1030

    Initial Changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dev-exercise
+# PHP/Laravel Development Exercise
+
+You'll use the boiler plate template provided to add the following functionality:
+
+* Add an additional checkbox that asks the user to "Confirm send".
+* Make all form fields required, and validate the email address.
+* Send an email on successfully completing the form (can be plain text).
+
+Fork the code from this repository to your own account, and upon completion, please send a pull request via GitHub with your code changes.
---
+++ app/resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="center-block">PHP / Laravel Challenge</h3>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Send an Email.</h1>
+            <p class="lead">Complete the form, and an email will be sent.</p>
+            
+            {!! Form::open(array('url' => '/')) !!}
+            
+            	<p>{{ Form::text('name', null, array('class' => 'form-control input-lg', 'placeholder' => 'Name')) }}</p>
+            
+            	<p>{{ Form::text('email', null, array('class' => 'form-control input-lg', 'placeholder' => 'Email Address')) }}</p>
+            	
+            	<p>{{ Form::submit('Submit', ['class' => 'btn btn-success btn-lg']) }}</p>
+            
+            {!! Form::close() !!}
+            
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>nucleus.com.au</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
